e6022705e92b78c67a03c135e04acace379eac60
Daniel <danielbeloded@gmail.com>
2023-04-11 17:53:27 +0300

    finish the app

---
+++ frontend/src/pages/ChatsPage/ChatsPage.jsx
@@ -0,0 +1,17 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId='402f4b43-9673-44e0-8e22-8e17b6f07927'
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
+
